c072a04ef22e2f95a69ec4638daf27958419da4e
dkb143 <dkb143@dkbvaghela>
2015-12-18 12:24:04 -0800

    add google analytics

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,102 +1,120 @@
 <!DOCTYPE html>
 <html lang="en"><head>
-<meta charset="UTF-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="description" content="V.Art Graphics provides graphic design, printing, packaging, stickers and wedding card services.">
-<meta name="keywords" content="Graphic Design, Printing, Packaging, Stickers, Wedding Cards">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<title><?= $title ?></title>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="V.Art Graphics provides graphic design, printing, packaging, stickers and wedding card services.">
+        <meta name="keywords" content="Graphic Design, Printing, Packaging, Stickers, Wedding Cards">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title><?= $title ?></title>
 
-<!-- Google fonts -->
-<link href="<?= base_url() ?>css/css.css" rel="stylesheet" type="text/css">
+        <!-- Google fonts -->
+        <link href="<?= base_url() ?>css/css.css" rel="stylesheet" type="text/css">
 
-<!-- font awesome -->
-<link href="<?= base_url() ?>css/font-awesome.min.css" rel="stylesheet">
+        <!-- font awesome -->
+        <link href="<?= base_url() ?>css/font-awesome.min.css" rel="stylesheet">
 
-<!-- bootstrap -->
-<link rel="stylesheet" href="<?= base_url() ?>css/bootstrap.css">
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="<?= base_url() ?>css/bootstrap.css">
 
-<!-- animate.css -->
-<link rel="stylesheet" href="<?= base_url() ?>css/animate.css">
-<link rel="stylesheet" href="<?= base_url() ?>css/set.css">
+        <!-- animate.css -->
+        <link rel="stylesheet" href="<?= base_url() ?>css/animate.css">
+        <link rel="stylesheet" href="<?= base_url() ?>css/set.css">
 
-<!-- gallery -->
-<link rel="stylesheet" href="<?= base_url() ?>css/blueimp-gallery.css">
+        <!-- gallery -->
+        <link rel="stylesheet" href="<?= base_url() ?>css/blueimp-gallery.css">
 
-<!-- favicon -->
-<link rel="shortcut icon" href="http://thebootstrapthemes.com/live/thebootstrapthemes-photography/images/favicon.ico" type="image/x-icon">
-<link rel="icon" href="http://thebootstrapthemes.com/live/thebootstrapthemes-photography/images/favicon.ico" type="image/x-icon">
+        <!-- favicon -->
+        <link rel="shortcut icon" href="http://thebootstrapthemes.com/live/thebootstrapthemes-photography/images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="http://thebootstrapthemes.com/live/thebootstrapthemes-photography/images/favicon.ico" type="image/x-icon">
 
 
-<link rel="stylesheet" href="<?= base_url() ?>css/style.css">
+        <link rel="stylesheet" href="<?= base_url() ?>css/style.css">
 
-<script type="text/javascript">
-var base_url="<?php echo base_url() ?>";
-</script>
+        <script type="text/javascript">
+            var base_url = "<?php echo base_url() ?>";
+        </script>
 
-<!-- jquery -->
-<script src="<?= base_url() ?>js/jquery.min.js"></script>
+        <!-- jquery -->
+        <script src="<?= base_url() ?>js/jquery.min.js"></script>
 
-<!-- wow script -->
-<script src="<?= base_url() ?>js/wow.js"></script>
+        <!-- wow script -->
+        <script src="<?= base_url() ?>js/wow.js"></script>
 
-<!-- boostrap -->
-<script src="<?= base_url() ?>js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- boostrap -->
+        <script src="<?= base_url() ?>js/bootstrap.min.js" type="text/javascript"></script>
 
-<!-- jquery mobile -->
-<script src="<?= base_url() ?>js/touchSwipe.js"></script>
-<script src="<?= base_url() ?>js/respond.js"></script>
+        <!-- jquery mobile -->
+        <script src="<?= base_url() ?>js/touchSwipe.js"></script>
+        <script src="<?= base_url() ?>js/respond.js"></script>
 
-<!-- gallery -->
-<script src="<?= base_url() ?>js/jquery.js"></script>
+        <!-- gallery -->
+        <script src="<?= base_url() ?>js/jquery.js"></script>
 
-<script src="<?= base_url() ?>js/jquery.validate.js"></script>
-<!-- custom script -->
-<script src="<?= base_url() ?>js/script.js"></script>
+        <script src="<?= base_url() ?>js/jquery.validate.js"></script>
+        <!-- custom script -->
+        <script src="<?= base_url() ?>js/script.js"></script>
 
 <!--<script src="Cyrus%20Studio_files/ga.js" async="" type="text/javascript"></script>
-<script src="Cyrus%20Studio_files/google_analytics_auto.js"></script></head>-->
+<script src="Cyrus%20Studio_files/google_analytics_auto.js"></script>-->
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-<body>
-<div class="topbar animated fadeInLeftBig"></div>
+            ga('create', 'UA-71561900-1', 'auto');
+            ga('send', 'pageview');
 
-<!-- Header Starts -->
-<div class="navbar-wrapper">
-      <div class="container">
+        </script>
+    </head>
 
-        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="top-nav">
-          <div class="container">
-            <div class="navbar-header">
-              <!-- Logo Starts -->
-              <a class="navbar-brand" href="#home"><img src="<?= base_url() ?>images/logo.jpg" alt="logo"></a>
-              <!-- #Logo Ends -->
+    <body>
+        <div class="topbar animated fadeInLeftBig"></div>
+
+        <!-- Header Starts -->
+        <div class="navbar-wrapper">
+            <div class="container">
+
+                <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="top-nav">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <!-- Logo Starts -->
+                            <a class="navbar-brand" href="#home"><img src="<?= base_url() ?>images/logo.jpg" alt="logo"></a>
+                            <!-- #Logo Ends -->
 
 
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                        </div>
+
+
+                        <!-- Nav Starts -->
+                        <div class="navbar-collapse  collapse">
+                            <ul class="nav navbar-nav navbar-right scroll">
+                                <li class="active"><a href="#home">Home</a></li>
+                                <li class=""><a href="#about">About</a></li>
+                                <li class=""><a href="#works">Works</a></li>
+                                <li class=""><a href="#partners">Partners</a></li>
+                                <li class=""><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                        <!-- #Nav Ends -->
+
+                    </div>
+                </div>
 
             </div>
-
-
-            <!-- Nav Starts -->
-            <div class="navbar-collapse  collapse">
-              <ul class="nav navbar-nav navbar-right scroll">
-                 <li class="active"><a href="#home">Home</a></li>
-                 <li class=""><a href="#about">About</a></li>
-                 <li class=""><a href="#works">Works</a></li>
-                 <li class=""><a href="#partners">Partners</a></li>
-                 <li class=""><a href="#contact">Contact</a></li>
-              </ul>
-            </div>
-            <!-- #Nav Ends -->
-
-          </div>
         </div>
-
-      </div>
-    </div>
-<!-- #Header Starts -->
+        <!-- #Header Starts -->
